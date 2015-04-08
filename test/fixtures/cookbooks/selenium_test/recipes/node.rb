7c1060783c39097cc35d709895b9bc547a9b04a0
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-07 21:03:27 -0700

    Test for centos 6

--- test/fixtures/cookbooks/selenium_test/recipes/node.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/node.rb
@@ -18,7 +18,7 @@ unless platform?('debian')
   }
 end
 
-unless platform_family?('rhel') && node['platform_version'] == '6.5'
+unless platform_family?('rhel') && node['platform_version'].split('.')[0] == '6'
   include_recipe 'chrome'
   capabilities <<  {
     browserName: 'chrome',
@@ -30,25 +30,26 @@ unless platform_family?('rhel') && node['platform_version'] == '6.5'
 end
 
 if platform?('windows')
-  # https://code.google.com/p/selenium/wiki/InternetExplorerDriver#Required_Configuration
-  node.set['ie']['enhanced_security_configuration'] = false
-  include_recipe 'ie::enhanced_security_configuration'
-
-  major_version = ie_version.split('.')[0].to_i
-
-  # On IE 7 or higher, you must set the Protected Mode settings for each zone to be the same value. The value can be on
-  # or off, as long as it is the same or every zone.
-  if major_version >= 7
-    node.set['ie']['zone']['internet'] = { '2500' => 0, '1400' => 0 } # enable both protected mode and javascript
-    include_recipe 'ie::security_zones'
-  end
-
-  # For IE 11 only, you will need to set a registry entry on the target computer so that the driver can maintain a
-  # connection to the instance of Internet Explorer it creates.
-  if major_version >= 11
-    node.set['ie']['feature_bfcache'] = true
-    include_recipe 'ie::feature_bfcache'
-  end
+  # TODO: get IE driver required configuration to work
+  # # https://code.google.com/p/selenium/wiki/InternetExplorerDriver#Required_Configuration
+  # node.set['ie']['enhanced_security_configuration'] = false
+  # include_recipe 'ie::enhanced_security_configuration'
+  #
+  # major_version = ie_version.split('.')[0].to_i
+  #
+  # # On IE 7 or higher, you must set the Protected Mode settings for each zone to be the same value. The value can be on
+  # # or off, as long as it is the same or every zone.
+  # if major_version >= 7
+  #   node.set['ie']['zone']['internet'] = { '2500' => 0, '1400' => 0 } # enable both protected mode and javascript
+  #   include_recipe 'ie::security_zones'
+  # end
+  #
+  # # For IE 11 only, you will need to set a registry entry on the target computer so that the driver can maintain a
+  # # connection to the instance of Internet Explorer it creates.
+  # if major_version >= 11
+  #   node.set['ie']['feature_bfcache'] = true
+  #   include_recipe 'ie::feature_bfcache'
+  # end
 
   capabilities <<  {
     browserName: 'internet explorer',
@@ -63,6 +64,7 @@ selenium_node 'selenium_node' do
   username 'Administrator' if platform?('windows')
   password 'password' if platform?('windows')
   capabilities capabilities
+  jvm_args '-Xms1024m'
   action :install
 end
 
--- test/integration/selenium_test/rspec/chrome_spec.rb
+++ test/integration/selenium_test/rspec/chrome_spec.rb
@@ -1,16 +1,18 @@
 require 'rspec_helper'
 
-describe 'Chrome Grid' do
-  before(:all) do
-    @selenium = Selenium::WebDriver.for(:remote, desired_capabilities: :chrome)
-  end
+unless `cat /etc/*-release` =~ /CentOS release 6/
+  describe 'Chrome Grid' do
+    before(:all) do
+      @selenium = Selenium::WebDriver.for(:remote, desired_capabilities: :chrome)
+    end
 
-  after(:all) do
-    @selenium.quit
-  end
+    after(:all) do
+      @selenium.quit
+    end
 
-  it 'Should return the-internet.herokuapp.com page' do
-    @selenium.get 'http://the-internet.herokuapp.com'
-    expect(@selenium.title).to eq('The Internet')
+    it 'Should return the-internet.herokuapp.com page' do
+      @selenium.get 'http://the-internet.herokuapp.com'
+      expect(@selenium.title).to eq('The Internet')
+    end
   end
 end
