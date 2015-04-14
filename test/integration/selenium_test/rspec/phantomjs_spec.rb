54e830a7531a76d37de2c6164eadef131f9d5452
Dennis Hoer <dennis.hoer@pearson.com>
2015-04-13 23:27:29 -0700

    Set display resolution in phantomjs tests

--- test/integration/selenium_test/rspec/phantomjs_spec.rb
+++ test/integration/selenium_test/rspec/phantomjs_spec.rb
@@ -2,15 +2,18 @@ require 'rspec_helper'
 
 describe 'PhantomJS Grid' do
   before(:all) do
-    @selenium = Selenium::WebDriver.for(:remote, desired_capabilities: :phantomjs)
+    @selenium = Selenium::WebDriver.for(:remote, url: 'http://localhost:8911')
+    @selenium.manage.window.size = Selenium::WebDriver::Dimension.new(1280, 1024)
   end
 
   after(:all) do
     @selenium.quit
   end
 
-  it 'Should return the-internet.herokuapp.com page' do
-    @selenium.get 'http://the-internet.herokuapp.com'
-    expect(@selenium.title).to eq('The Internet')
+  it 'Should return display resolution of 1280x1024' do
+    @selenium.get 'http://google.com/'
+    @selenium.save_screenshot('screenshot.png')
+    file_info = `file screenshot.png`.chomp
+    expect(file_info).to eq('screenshot.png: PNG image data, 1280 x 1024, 8-bit/color RGBA, non-interlaced')
   end
 end
