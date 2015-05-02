74285702cb24e5ec380250e3bec72e7f8f3f4015
Dennis Hoer <dennis.hoer@pearson.com>
2015-05-02 02:14:13 -0700

    Add ie config example to test fixture

--- test/fixtures/cookbooks/selenium_test/recipes/node.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/node.rb
@@ -30,26 +30,41 @@ unless platform_family?('rhel') && node['platform_version'].split('.')[0] == '6'
 end
 
 if platform?('windows')
-  # TODO: get IE driver required configuration to work
-  # # https://code.google.com/p/selenium/wiki/InternetExplorerDriver#Required_Configuration
-  # node.set['ie']['enhanced_security_configuration'] = false
-  # include_recipe 'ie::enhanced_security_configuration'
-  #
-  # major_version = ie_version.split('.')[0].to_i
-  #
-  # # On IE 7 or higher, you must set the Protected Mode settings for each zone to be the same value.
-  # # The value can be on or off, as long as it is the same or every zone.
-  # if major_version >= 7
-  #   node.set['ie']['zone']['internet'] = { '2500' => 0, '1400' => 0 } # enable both protected mode and javascript
-  #   include_recipe 'ie::security_zones'
-  # end
-  #
-  # # For IE 11 only, you will need to set a registry entry on the target computer so that the driver can maintain a
-  # # connection to the instance of Internet Explorer it creates.
-  # if major_version >= 11
-  #   node.set['ie']['feature_bfcache'] = true
-  #   include_recipe 'ie::feature_bfcache'
-  # end
+  # https://code.google.com/p/selenium/wiki/InternetExplorerDriver#Required_Configuration
+  major_version = ie_version.split('.')[0].to_i
+
+  # For IE 11 only, you will need to set a registry entry on the target computer so that the driver can maintain a
+  # connection to the instance of Internet Explorer it creates.
+  include_recipe 'ie::bfcache' if major_version >= 11
+
+  include_recipe 'ie::esc' # disable annoying enhanced security configuration
+
+  include_recipe 'ie::firstrun' if major_version == 9 || major_version == 8  # disable first run setup pop-up
+
+  # On IE 7 or higher, you must set the Protected Mode settings for each zone to be the same value.
+  # The value can be on or off, as long as it is the same or every zone.
+  if major_version >= 7
+    node.set['ie']['zone']['internet'] = {
+      '1400' => 0, # enable active scripting
+      '2500' => 0 # enable protected mode
+    }
+
+    node.set['ie']['zone']['local_internet'] = {
+      '2500' => 0 # enable protected mode
+    }
+
+    node.set['ie']['zone']['trusted_sites'] = {
+      '2500' => 0 # enable protected mode
+    }
+
+    node.set['ie']['zone']['restricted_sites'] = {
+      '2500' => 0 # enable protected mode
+    }
+
+    include_recipe 'ie::zone'
+  end
+
+  include_recipe 'ie::zoom' # set zoom level to 100%
 
   capabilities << {
     browserName: 'internet explorer',
