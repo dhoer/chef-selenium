0513db1af52b8a56196dc1a62c625a88f3e3c26a
Dennis Hoer <dennis.hoer@pearson.com>
2015-06-29 12:43:09 -0700

    Add powershell_version

--- libraries/default.rb
+++ libraries/default.rb
@@ -172,3 +172,11 @@ end
 def mac_domain(name)
   "org.seleniumhq.#{name}"
 end
+
+# Fixes #15: Drivers not copied to /selenium/drivers/ folders on Windows 7
+def powershell_version
+  out = shell_out!('powershell.exe $host.version.major')
+  out.to_i
+rescue # powershell not installed
+  -1
+end
