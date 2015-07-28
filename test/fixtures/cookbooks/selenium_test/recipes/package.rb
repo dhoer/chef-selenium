704df29b116410b0e20d1d5140bb15076b7c2067
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-27 19:47:34 -0700

    Run apt-get update at compile time on integration test

--- test/fixtures/cookbooks/selenium_test/recipes/package.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/package.rb
@@ -1,6 +1,8 @@
 case node[:platform_family]
 when 'debian'
-  # include_recipe 'apt'
+  execute 'sudo apt-get update' do
+    action :nothing
+  end.run_action(:run) # firefox runs at compile time and firefox package is not up to date on Ubuntu 14.04-1
 when 'rhel'
   include_recipe 'yum'
 when 'mac_os_x'
