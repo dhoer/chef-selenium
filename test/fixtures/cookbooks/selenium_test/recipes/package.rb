bd2570c607d96bc442fbed2c1de8d3828959be4f
Dennis Hoer <dennis.hoer@pearson.com>
2015-08-01 11:38:31 -0700

    Add ffi package dependencies

--- test/fixtures/cookbooks/selenium_test/recipes/package.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/package.rb
@@ -1,10 +1,24 @@
 case node[:platform_family]
 when 'debian'
+  # firefox runs at compile time and firefox package is not up to date on Ubuntu 14.04-1
   execute 'sudo apt-get update' do
     action :nothing
-  end.run_action(:run) # firefox runs at compile time and firefox package is not up to date on Ubuntu 14.04-1
+  end.run_action(:run)
+  # selenium-webdriver includes ffi which requires the following dependencies
+  package 'gcc' do
+    action :nothing
+  end.run_action(:install)
+  package 'g++' do
+    action :nothing
+  end.run_action(:install)
+  package 'libffi-dev' do
+    action :nothing
+  end.run_action(:install)
 when 'rhel'
   include_recipe 'yum'
+  # selenium-webdriver includes ffi which requires the following dependencies
+  package 'gcc'
+  package 'libffi-devel'
 when 'mac_os_x'
   include_recipe 'dmg'
   include_recipe 'homebrew'
