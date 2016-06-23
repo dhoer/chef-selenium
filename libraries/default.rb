901302b63b32b24567eedb9f8fc7d33f6276b7ca
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-22 23:48:14 -0700

    Fix rubocop offenses

--- libraries/default.rb
+++ libraries/default.rb
@@ -85,9 +85,9 @@ def selenium_autologon(username, password, domain = nil)
 end
 
 def selenium_systype
-  return 'systemd' if ::File.exist?(Chef.path_to("/proc/1/comm")) &&
-    ::File.open(Chef.path_to("/proc/1/comm")).gets.chomp == "systemd"
-  return 'upstart' if ::File.exist?(Chef.path_to("/sbin/initctl"))
+  return 'systemd' if ::File.exist?(Chef.path_to('/proc/1/comm')) &&
+                      ::File.open(Chef.path_to('/proc/1/comm')).gets.chomp == 'systemd'
+  return 'upstart' if ::File.exist?(Chef.path_to('/sbin/initctl'))
   'sysvinit'
 end
 
