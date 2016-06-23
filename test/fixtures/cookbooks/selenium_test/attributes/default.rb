c9a6804a9d530724b16eb3514af374a5c16d0bce
Dennis Hoer <dennis.hoer@pearson.com>
2016-06-22 18:21:31 -0700

    Set test username and password to vagrant

--- test/fixtures/cookbooks/selenium_test/attributes/default.rb
+++ test/fixtures/cookbooks/selenium_test/attributes/default.rb
@@ -1,2 +1,2 @@
-default['selenium_test']['username'] = ''
-default['selenium_test']['password'] = ''
+default['selenium_test']['username'] = 'vagrant'
+default['selenium_test']['password'] = 'vagrant'
