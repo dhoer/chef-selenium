7f6244d5de2d4466731cfae7b931cc232efbfe4a
Dennis Hoer <dennis.hoer@pearson.com>
2016-07-03 18:48:04 -0700

    Add make

--- test/fixtures/cookbooks/selenium_test/recipes/package.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/package.rb
@@ -1,9 +1,13 @@
+# selenium-webdriver includes ffi which requires the following dependencies
 case node[:platform_family]
-when 'debian', 'rhel'
-  # selenium-webdriver includes ffi which requires the following dependencies
+when 'debian'
   %w(gcc libffi-dev make).each do |pkg|
     package pkg do
       action :nothing
     end.run_action(:install)
+  end
+when 'rhel'
+  %w(gcc libffi-devel make).each do |pkg|
+    package pkg
   end
 end
