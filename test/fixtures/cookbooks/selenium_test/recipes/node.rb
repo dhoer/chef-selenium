37e6ad4640f2d85ad627d5af9ae9e8fbce81d40f
Dennis Hoer <dennis.hoer@pearson.com>
2016-09-23 11:57:54 -0700

    Another attempt at Windows verification

--- test/fixtures/cookbooks/selenium_test/recipes/node.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/node.rb
@@ -18,17 +18,3 @@ node.override['selenium']['node']['username'] = node['selenium_test']['username'
 node.override['selenium']['node']['password'] = node['selenium_test']['password']
 
 include_recipe 'selenium::node'
-
-if platform?('windows')
-  # Call windows_screenresolution after selenium_node because windows_screenresolution
-  # will override auto-login created by selenium_node.
-  node.override['windows_screenresolution']['username'] = node['selenium_test']['username']
-  node.override['windows_screenresolution']['password'] = node['selenium_test']['password']
-  node.override['windows_screenresolution']['width'] = 1440
-  node.override['windows_screenresolution']['height'] = 900
-
-  # Meets windows password policy requirements for a new user
-  node.override['windows_screenresolution']['rdp_password'] = 'S6M;b.v+{DTYAQW4'
-
-  include_recipe 'windows_screenresolution'
-end
