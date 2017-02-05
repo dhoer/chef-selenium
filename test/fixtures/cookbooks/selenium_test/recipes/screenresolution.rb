97441cd644c9e2ca17a3228a4e1794c8c6284936
Dennis Hoer <dennis.hoer@pearson.com>
2017-02-04 19:55:25 -0700

    Update screenresolution to use resource

--- test/fixtures/cookbooks/selenium_test/recipes/screenresolution.rb
+++ test/fixtures/cookbooks/selenium_test/recipes/screenresolution.rb
@@ -1,13 +1,10 @@
 if platform?('windows')
   # Call windows_screenresolution after selenium_node because windows_screenresolution
   # will override auto-login created by selenium_node.
-  node.override['windows_screenresolution']['username'] = node['selenium_test']['username']
-  node.override['windows_screenresolution']['password'] = node['selenium_test']['password']
-  node.override['windows_screenresolution']['width'] = 1440
-  node.override['windows_screenresolution']['height'] = 900
-
-  # Meets windows password policy requirements for a new user
-  node.override['windows_screenresolution']['rdp_password'] = 'S6M;b.v+{DTYAQW4'
-
-  include_recipe 'windows_screenresolution'
+  windows_screenresolution node['selenium_test']['username'] do
+    password node['selenium_test']['password']
+    width 1440
+    height 900
+    rdp_password 'S6M;b.v+{DTYAQW4' # Meets windows password policy requirements for a new user
+  end
 end
